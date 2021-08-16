7e4f095d007158fdf889518b3a967b8284ab8532
l3ttuce <l3ttuce@localhost.localdomain>
2021-08-15 22:10:00 -0400

    Created/Modified files
    Created new files (Dictionary...) and modified Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,47 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException {
+        dictionaryLength();
     }
+
+    // 14 - Write code to read a dictionary from a file, then prompt the ser for two words and tell the user how
+    // many words in the dictionary fall between those two words.
+    public static void dictionaryLength() throws FileNotFoundException {
+        List<String> list = new LinkedList<>();
+        Scanner scanner = new Scanner(new File("/home/l3ttuce/IdeaProjects/Exercises_Searching-Sorting/Dictionary.txt"));
+        Scanner console = new Scanner(System.in);
+
+        String word1 = "";
+        String word2 = "";
+        System.out.println("Type two words: ");
+        String lineInput = console.nextLine();
+        Scanner line = new Scanner(lineInput);
+        while(line.hasNext()){
+            word1 = line.next();
+            word2 = line.next();
+        }
+        System.out.println(word1 + " " + word2);
+
+        // need to store every word in Dictionary.txt in a List - or an array -
+        // to then check how many words in between the two user input
+
+        // wil try to use recursion for going through the file
+
+        while(scanner.hasNextLine()){
+            Scanner lineFile = new Scanner(scanner.nextLine());
+            while(lineFile.hasNext()){
+                list.add(lineFile.next());
+            }
+        }
+        System.out.println(list);
+    }
+
 }
